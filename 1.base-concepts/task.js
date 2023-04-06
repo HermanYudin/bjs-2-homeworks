b86b5d7c92368874ad8f468e731d7fe831aa4256
HermanYudin <hermanhockey1994@yandex.ru>
2023-04-06 18:38:23 +0300

    Небольшие исправления

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -10,23 +10,20 @@ function solveEquation(a, b, c) {
       return [];
   }
 
-  if (d === 0) {
+  else if(d === 0) {
       return [-b / (2 * a)];
-  } else if (d > 0) {
+  } else (d > 0) {
       return [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
   }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let S;
-  let P;
-  let n;
 
   percent = percent / 100 / 12;
   amount = amount - contribution;
-  S = amount;
-  P = percent;
-  n = countMonths;
+  let S = amount;
+  let P = percent;
+  let n = countMonths;
 
   let payments = S * (P + (P / (((1 + P) ** n) - 1)));
   let summ = payments * n;
